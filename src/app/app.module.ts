1f1706ccc004c4685b83f6163bb421a1578a8f7e
KhalefAhmed <46876471+KhalefAhmed@users.noreply.github.com>
2022-09-25 20:30:10 +0200

    2 files modified, 5 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ServerComponent } from './components/server/server.component';
+import { ServersComponent } from './components/servers/servers.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
